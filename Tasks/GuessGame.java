7b693d46b9768f016bb37aa86c883d39c58bed51
Anuj Singh Sengar <131253614+anujsinghsengar01@users.noreply.github.com>
2025-11-29 21:56:12 +0530

    Improve GuessGame logic and user feedback
    Refactor guessing game logic and improve output messages.

--- Tasks/GuessGame.java
+++ Tasks/GuessGame.java
@@ -5,36 +5,36 @@ public class GuessGame{
 
         Random rand= new Random();
         Scanner sc= new Scanner(System.in);
-        int randomNumber = rand.nextInt(1,101); // to ger random number btw 1 to 100
-        System.out.println("GUESS THE NUMBER BTW 1-100");
-        
-        int guess= sc.nextInt(); // user in
+        int randomNumber = rand.nextInt(1,101); // to get random number btw 1 to 100
+        int guess=101; // Just to initialize (as random num cannt be 101
         
         int attempts = 10;  // max attempts to guess 
-        System.out.println(randomNumber); 
+        System.out.println(randomNumber); //cheat code 
+        System.out.println("GUESS THE NUMBER BTW 1-100");
 
         while(guess!=randomNumber && attempts!=0){
             
-            System.out.println("GUESS THE NUMBER BTW 1-100");
             guess= sc.nextInt();
-            attempts--; // WRONG GUESS 
-            
-
+            attempts--;
+        
             if(guess>randomNumber){
-                System.out.println(" GUESS WAS HIGGER !! ");
+                System.out.println(" GUESS WAS HIGGER !! TRY AGAIN ");
+                System.out.println("YOU HAVE "+attempts+" ATTEMPTS LEFT !");
+                
             }
-            else {
-                System.out.println(" GUESS WAS LOWER !! "); 
+            else if(guess<randomNumber){
+                System.out.println(" GUESS WAS LOWER !! TRY AGAIN "); 
+                System.out.println("YOU HAVE "+attempts+" ATTEMPTS LEFT !");
             }
-            System.out.println("YOU HAVE "+attempts+" ATTEMPTS LEFT !");
+            
               
         }
-        if(attempts==0){
-            System.out.println("YOU LOSE ! NUMBER WAS "+ randomNumber); // ALL ATTEMPTS USED 
+        if(attempts==0 && guess!=randomNumber){
+            System.out.println("YOU LOSE ! NUMBER WAS "+ randomNumber); // ALL ATTEMPTS USED (wrong guess)
         }
         else {
-            System.out.println("YOU WIN YOU GUESSED THE NUMBER"+randomNumber); // SUCCESSFULLLY GUESED THE NUMMBER
-            System.out.println("YOU HAVE USED "+(11-attempts)+" ATTEMPTS TO ANS !"); //ATTEMPTS TOOK
+            System.out.println("YOU WIN YOU GUESSED THE NUMBER : "+randomNumber); // SUCCESSFULLLY GUESED THE NUMMBER
+            System.out.println("YOU HAVE USED "+(10-attempts)+" ATTEMPTS TO ANS !"); //ATTEMPTS TOOK
             System.out.println("YOUR SCORE IS : " +(attempts+1)*10+" POINTS ! "); //SCORE
 
         }
