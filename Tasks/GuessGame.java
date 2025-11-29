3152ee3efc068ab63fab629e484d6ad0fae6f970
Anuj Singh Sengar <131253614+anujsinghsengar01@users.noreply.github.com>
2025-11-29 22:09:04 +0530

    Fix typos in GuessGame.java output messages

--- Tasks/GuessGame.java
+++ Tasks/GuessGame.java
@@ -6,10 +6,23 @@ public class GuessGame{
         Random rand= new Random();
         Scanner sc= new Scanner(System.in);
         int randomNumber = rand.nextInt(1,101); // to get random number btw 1 to 100
-        int guess=101; // Just to initialize (as random num cannt be 101
-        
         int attempts = 10;  // max attempts to guess 
+        int guess = 101; // just to initialize (as random num cannot be 101)
         System.out.println(randomNumber); //cheat code 
+
+
+        //  INTRODUCTON AND RULES
+        System.out.println("==========================================");
+        System.out.println("          🎯 WELCOME TO GUESS-GAME 🎯");
+        System.out.println("==========================================");
+        System.out.println("👋 Hello Player!");
+        System.out.println(" I have selected a secret number between 1 - 100.");
+        System.out.println(" You have 10 attempts to guess it correctly.");
+        System.out.println(" Points Based On Attempts.");
+        System.out.println(" Hints: I will tell you if your guess is TOO HIGH or TOO LOW.");
+        System.out.println("==========================================");
+        System.out.println("👉 GAME STARTS NOW...  All the best!");
+        System.out.println("==========================================\n");
         System.out.println("GUESS THE NUMBER BTW 1-100");
 
         while(guess!=randomNumber && attempts!=0){
@@ -18,7 +31,7 @@ public class GuessGame{
             attempts--;
         
             if(guess>randomNumber){
-                System.out.println(" GUESS WAS HIGGER !! TRY AGAIN ");
+                System.out.println(" GUESS WAS HIGHER !! TRY AGAIN ");
                 System.out.println("YOU HAVE "+attempts+" ATTEMPTS LEFT !");
                 
             }
