3ddb7e829e40dcd9a1d497db6ba6e70f626e6246
Anuj Singh Sengar <131253614+anujsinghsengar01@users.noreply.github.com>
2025-11-29 21:40:08 +0530

    Implement number guessing game logic

--- Tasks/GuessGame.java
+++ Tasks/GuessGame.java
@@ -1,1 +1,48 @@
-#
+import java.util.Scanner;
+import java.util.Random;
+public class GuessGame{
+    public static void main(String args[]){
+
+        Random rand= new Random();
+        Scanner sc= new Scanner(System.in);
+        int randomNumber = rand.nextInt(1,101); // to ger random number btw 1 to 100
+        System.out.println("GUESS THE NUMBER BTW 1-100");
+        
+        int guess= sc.nextInt(); // user in
+        
+        int attempts = 10;  // max attempts to guess 
+        System.out.println(randomNumber); 
+
+        while(guess!=randomNumber && attempts!=0){
+            
+            System.out.println("GUESS THE NUMBER BTW 1-100");
+            guess= sc.nextInt();
+            attempts--; // WRONG GUESS 
+            
+
+            if(guess>randomNumber){
+                System.out.println(" GUESS WAS HIGGER !! ");
+            }
+            else {
+                System.out.println(" GUESS WAS LOWER !! "); 
+            }
+            System.out.println("YOU HAVE "+attempts+" ATTEMPTS LEFT !");
+              
+        }
+        if(attempts==0){
+            System.out.println("YOU LOSE ! NUMBER WAS "+ randomNumber); // ALL ATTEMPTS USED 
+        }
+        else {
+            System.out.println("YOU WIN YOU GUESSED THE NUMBER"+randomNumber); // SUCCESSFULLLY GUESED THE NUMMBER
+            System.out.println("YOU HAVE USED "+(11-attempts)+" ATTEMPTS TO ANS !"); //ATTEMPTS TOOK
+            System.out.println("YOUR SCORE IS : " +(attempts+1)*10+" POINTS ! "); //SCORE
+
+        }
+
+
+    
+        sc.close();
+
+
+    }
+}
